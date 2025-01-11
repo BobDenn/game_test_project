4d66666f98104297d61a2feb0a6ba20e4d87cab6
dengkuo <1137005673@qq.com>
2025-01-11 13:45:24 +0800

    build application

--- Scripts/SkipGuidance.py
+++ Scripts/SkipGuidance.py
@@ -5,7 +5,12 @@ import pyautogui as gui
 time.sleep(3)
 
 if(gui.onScreen(1600, 750)):
-    gui.click(337, 531, button = "left", clicks = 3, interval = 11)
+    # 回答三个问题
+    #gui.click(337, 531, button = "left", clicks = 3, interval = 11)
+
+    gui.moveTo(821, 666,duration=0.5)
+    gui.click()
+
     print("你在游戏里很安全")
 else:
     # Out of the Screen # 保护
